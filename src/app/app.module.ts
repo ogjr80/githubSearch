e0133980720f3f3bb8601f876a0f3f2a8ba2ed24
Gideon <ogjr80@gmail.com>
2017-04-30 00:24:19 +0200

    The first gihubSearch app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }      from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {GithubService} from './services/github.service';
 
 import { AppComponent }  from './app.component';
-
+import {ProfileComponent} from './components/profile.component';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
+  declarations: [ AppComponent, ProfileComponent ],
+  providers: [GithubService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
